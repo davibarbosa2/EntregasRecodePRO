c9ba0361c221b90edcee731f296a185544856f26
Davi Barbosa <davibarbosa019@gmail.com>
2021-02-03 23:23:31 -0300

    adição de dois campos para a consulta do CEP

--- entregaveisRECODE/nodeAvancado/app.js
+++ entregaveisRECODE/nodeAvancado/app.js
@@ -7,22 +7,20 @@ const buscaCep = require("./src/functions/buscaCep");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 app.set("view engine", "ejs");
 
 app.set("views", "./src/views");
 
-
 app.get("/", (req, res) => {
-	
 	res.render("index");
 });
 
 app.post("/envia-cep", async (req, res) => {
-	const { cep } = req.body;
-	const resultado = await buscaCep(cep);
+	const { uf, localidade, logradouro } = req.body;
+	const resultado = await buscaCep(uf, localidade, logradouro);
 
 	res.render("resultado", { dado: resultado });
+	
 });
 
 app.listen(3333);
--- entregaveisRECODE/nodeAvancado/src/functions/buscaCep.js
+++ entregaveisRECODE/nodeAvancado/src/functions/buscaCep.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
-module.exports = async function buscaCep(cep) {
-	const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+module.exports = async function buscaCep(uf,localidade,ddd) {
+	const response = await fetch(`https://viacep.com.br/ws/${uf}/${localidade}/${ddd}/json/`);
 	const json = await response.json();
 
 	return json;
