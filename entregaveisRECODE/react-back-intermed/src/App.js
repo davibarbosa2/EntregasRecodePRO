e98983e36acde5120918c326dbb66121eb4301dc
Davi Barbosa <davibarbosa019@gmail.com>
2021-02-12 03:01:07 -0300

    implementação de lazy loading ao projeto

--- entregaveisRECODE/react-back-intermed/src/App.js
+++ entregaveisRECODE/react-back-intermed/src/App.js
@@ -2,14 +2,21 @@ import React, { Component } from "react";
 import "./App.css";
 import { Provider } from "react-redux";
 import { HashRouter as Router, Route } from "react-router-dom";
+import Spinner from './components/layout/Spinner'
 
-import Navbar from "./components/layout/Navbar";
-import Footer from "./components/layout/Footer";
-
-import Landing from "./components/home/Landing";
 import Movie from "./components/home/Movie";
 
 import store from "./store";
+
+import {lazy, Suspense} from 'react';
+//import Navbar from "./components/layout/Navbar"; foi aplicado o lazy loading
+//import Footer from "./components/layout/Footer"; foi aplicado o lazy loading
+//import Landing from "./components/home/Landing"; foi aplicado o lazy loading
+
+const Navbar = lazy(() => import('./components/layout/Navbar')) //aplicação lazy loading 
+const Footer = lazy(() => import('./components/layout/Footer')) //aplicação lazy loading 
+const Landing = lazy(() => import('./components/home/Landing')) //aplicação lazy loading 
+
 
 class App extends Component {
 	render() {
@@ -17,10 +24,20 @@ class App extends Component {
 			<Provider store={store}>
 				<Router>
 					<div>
+					<Suspense fallback={ <Spinner/> }>
 						<Navbar />
+                    </Suspense>
+
+					<Suspense fallback={ <Spinner/> }>
 						<Route exact path="/" component={Landing} />
+                    </Suspense>
+
 						<Route exact path="/movie/:id" component={Movie} />
+
+					<Suspense fallback={ <Spinner/> }>
 						<Footer />;
+                    </Suspense>
+						
 					</div>
 				</Router>
 			</Provider>
